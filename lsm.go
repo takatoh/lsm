038ed664f1bd08eacbc6dcba1d344512c3c51749
takatoh <takatoh.m@gmail.com>
2018-12-16 12:42:29 +0900

    lsm.go: Fix bugs.

--- lsm.go
+++ lsm.go
@@ -5,13 +5,13 @@ func LSM1(xs, ys []float64) (float64, float64) {
 
 	n := len(xs)
 	for i := 0; i < n; i++ {
-		sum_xy += x[i] * y[i]
-		sum_x += x[i]
-		sum_y += y[i]
-		sum_x2 += x[i] * x[i]
+		sum_xy += xs[i] * ys[i]
+		sum_x += xs[i]
+		sum_y += ys[i]
+		sum_x2 += xs[i] * xs[i]
 	}
 
-	nf = float64(n)
+	nf := float64(n)
 	a := (nf * sum_xy - sum_x * sum_y) / (nf * sum_x2 - sum_x * sum_x)
 	b := (sum_x2 * sum_y - sum_xy * sum_x) / (nf * sum_x2 - sum_x * sum_x)
 
